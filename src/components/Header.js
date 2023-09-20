eeb1d7aa2eb8c6152aa97dc8511cb95282836696
Faraaz Electricwala <61732459+faraaz-e@users.noreply.github.com>
2023-09-20 17:04:09 +0530

    Updated UI
    Add icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,20 +3,9 @@ const Header = () => {
     <div className="grid grid-flow-col">
       <div className="flex col-span-2 py-2 px-5 items-center">
         <button className="hidden md:block">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="w-6 h-6 text-white"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-            />
-          </svg>
+          <span className="material-symbols-outlined text-white align-middle">
+            menu
+          </span>
         </button>
 
         <img
@@ -25,78 +14,40 @@ const Header = () => {
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/YouTube_dark_logo_2017.svg/1280px-YouTube_dark_logo_2017.svg.png"
         />
 
-        <sup className="text-zinc-400 pl-2">IN</sup>
+        <sup className="text-zinc-400 pl-1">IN</sup>
       </div>
 
       <div className="col-span-8 flex justify-center py-2">
         <input
           type="text"
-          className="py-2 px-4 border border-solid border-zinc-800 bg-black text-zinc-800 focus:border-blue-800 rounded-l-full w-1/2"
+          className="py-1 px-4 border border-solid border-zinc-800 bg-black text-white focus:border-blue-800 rounded-l-full w-1/2"
           placeholder="Search"
         />
 
-        <button className="py-2 px-5 border border-solid border-zinc-800 bg-zinc-800 hover:bg-zinc-700 rounded-r-full">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="w-5 h-5 text-white"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-            />
-          </svg>
+        <button className="py-1 px-4 border border-solid border-zinc-800 bg-zinc-800 hover:bg-zinc-700 rounded-r-full">
+          <span className="material-symbols-outlined text-white align-middle">
+            search
+          </span>
         </button>
 
-        <button className="mx-3 p-3 bg-zinc-800 hover:bg-zinc-700 rounded-full text-white hidden md:block">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="w-5 h-5"
-          >
-            <path d="M8.25 4.5a3.75 3.75 0 117.5 0v8.25a3.75 3.75 0 11-7.5 0V4.5z" />
-            <path d="M6 10.5a.75.75 0 01.75.75v1.5a5.25 5.25 0 1010.5 0v-1.5a.75.75 0 011.5 0v1.5a6.751 6.751 0 01-6 6.709v2.291h3a.75.75 0 010 1.5h-7.5a.75.75 0 010-1.5h3v-2.291a6.751 6.751 0 01-6-6.709v-1.5A.75.75 0 016 10.5z" />
-          </svg>
+        <button className="mx-3 p-2 bg-zinc-800 hover:bg-zinc-700 rounded-full text-white hidden md:block">
+          <span className="material-symbols-outlined text-white align-middle">
+            mic
+          </span>
         </button>
       </div>
 
       <div className="p-2 col-span-2 flex justify-center items-center space-x-5">
         <button className="hidden md:block">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="w-6 h-6 text-white hidden md:block"
-          >
-            <path
-              strokeLinecap="round"
-              d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"
-            />
-          </svg>
+          <span className="material-symbols-outlined text-white align-middle">
+            videocam
+          </span>
         </button>
 
         <button>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="w-5 h-5 text-white"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
-            />
-          </svg>
+          <span className="material-symbols-outlined text-white align-middle">
+            notifications
+          </span>
         </button>
 
         <img
