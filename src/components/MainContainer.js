3bf0a3ed91283f7b8304829ab9c4b3e0dede0377
Faraaz Electricwala <61732459+faraaz-e@users.noreply.github.com>
2023-09-20 17:05:02 +0530

    Updated UI

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import ButtonList from './ButtonList'
-import VideoContainer from './VideoContainer'
+import React from "react";
+import ButtonList from "./ButtonList";
+import VideoContainer from "./VideoContainer";
 
 const MainContainer = () => {
   return (
-    <div>
-        <ButtonList />
-        <VideoContainer />
+    <div className="col-span-10">
+      <ButtonList />
+      <VideoContainer />
     </div>
-  )
-}
+  );
+};
 
-export default MainContainer
+export default MainContainer;
