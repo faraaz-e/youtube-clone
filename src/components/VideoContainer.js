ffe1d25298613223ab5a91bb31da6b97ff6d1ba0
Faraaz Electricwala <61732459+faraaz-e@users.noreply.github.com>
2023-09-21 17:02:27 +0530

    Fetching data from API

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,7 +1,28 @@
-const VideoContainer = () => {
-  return (
-    <div>VideoContainer</div>
-  )
-}
+import { useEffect, useState } from "react";
+import { YOUTUBE_VIDEOS_API } from "../utils/constants";
+import VideoCard from "./VideoCard";
 
-export default VideoContainer
+const VideoContainer = () => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    getVideos();
+  }, []);
+
+  const getVideos = async () => {
+    const data = await fetch(YOUTUBE_VIDEOS_API);
+    const jsonData = await data.json();
+    setVideos(jsonData.items);
+    console.log(videos);
+  };
+
+  return (
+    <div className="flex flex-wrap">
+      {videos.map((video) => (
+        <VideoCard key={videos.id} info={video} />
+      ))}
+    </div>
+  );
+};
+
+export default VideoContainer;
