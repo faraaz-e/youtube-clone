7e356402b4cbd6ee7395770bc946844285861ee4
Faraaz Electricwala <61732459+faraaz-e@users.noreply.github.com>
2023-09-21 17:01:22 +0530

    API updated

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
 export const YOUTUBE_VIDEOS_API =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=" +
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=" +
   process.env.REACT_APP_GOOGLE_API_KEY;
