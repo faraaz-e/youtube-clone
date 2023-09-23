fc17db336d15941ff3609d9eef37fab866050723
Faraaz Electricwala <61732459+faraaz-e@users.noreply.github.com>
2023-09-23 16:13:39 +0530

    Adding Higher Order Component

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,26 +1,35 @@
 const VideoCard = ({ info }) => {
   console.log(info);
-  // const { snippet, statistics } = info;
-  // const { channelTitle, title, thumbnails } = snippet;
+  const { snippet, statistics } = info;
+  const { channelTitle, title, thumbnails } = snippet;
 
   return (
     <div className="p-1 m-1 w-72">
       <img
         alt="thumbnail"
         className="rounded-lg"
-        src={info?.snippet?.thumbnails?.high?.url}
+        src={snippet?.thumbnails?.high?.url}
       />
       <div className="">
         <label className="text-white font-semibold">
-          {info?.snippet?.title}
+          {snippet?.title}
         </label>
         <p className="text-zinc-400 font-medium text-sm">
-          {info?.snippet?.channelTitle}
+          {snippet?.channelTitle}
         </p>
         <p className="text-zinc-400 font-medium text-sm">
-          {info?.statistics?.viewCount} views・{info?.snippet?.publishedAt}
+          {statistics?.viewCount} views・{snippet?.publishedAt}
         </p>
       </div>
+    </div>
+  );
+};
+
+// Higher Order Component
+export const AdVideoCard = ({ info }) => {
+  return (
+    <div className="p-1 m-1 border border-zinc-800">
+      <VideoCard info={info} />
     </div>
   );
 };
