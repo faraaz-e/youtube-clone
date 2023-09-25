d454bdb32bbee016f13fb4ccf1881b24332b9b36
Faraaz Electricwala <61732459+faraaz-e@users.noreply.github.com>
2023-09-25 20:22:38 +0530

    Code refactoring

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -14,14 +14,13 @@ const VideoContainer = () => {
     const data = await fetch(YOUTUBE_VIDEOS_API);
     const jsonData = await data.json();
     setVideos(jsonData.items);
-    console.log(videos);
   };
 
   return (
     <div className="flex flex-wrap">
       {videos[0] && <AdVideoCard info={videos[30]} />}
       {videos.map((video) => (
-        <Link key={videos.id} to={"/watch?v=" + video.id}>
+        <Link key={video.id} to={"/watch?v=" + video.id}>
           <VideoCard info={video} />
         </Link>
       ))}
