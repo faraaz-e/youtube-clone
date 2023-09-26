738dacdaef2fe7b676036be6323668f71cefdbfc
Faraaz Electricwala <61732459+faraaz-e@users.noreply.github.com>
2023-09-26 15:50:54 +0530

    Added live chat count to be configurable

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,8 @@
 export const YOUTUBE_VIDEOS_API =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=" +
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   process.env.REACT_APP_GOOGLE_API_KEY;
 
 export const YOUTUBE_SEARCH_API =
   "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+
+export const LIVE_CHAT_COUNT = 25;
