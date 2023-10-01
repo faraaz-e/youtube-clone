94b2efbbe379fc3b0ff040cc4b8b5fe8b71714d4
Faraaz Electricwala <61732459+faraaz-e@users.noreply.github.com>
2023-10-01 21:11:55 +0530

    Input field added for live chat

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,10 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ChatMessage from "./ChatMessage";
 import { useDispatch, useSelector } from "react-redux";
 import { addMessage } from "../utils/chatSlice";
 import { generateRandomMessage, generateRandomName } from "../utils/helper";
 
 const LiveChat = () => {
+  const [liveMessage, setLiveMessage] = useState("");
   const dispatch = useDispatch();
   const chatMessages = useSelector((store) => store.chat.messages);
 
@@ -25,11 +26,38 @@ const LiveChat = () => {
   }, []);
 
   return (
-    <div className="mx-5 p-2 w-96 h-[500px] border border-zinc-800 rounded-lg text-white overflow-y-scroll flex flex-col-reverse">
-      {chatMessages.map((c, i) => (
-        <ChatMessage key={i} name={c.name} message={c.message} />
-      ))}
-    </div>
+    <>
+      <div className="mx-5 p-2 w-96 h-[500px] border border-zinc-800 rounded-lg text-white overflow-y-scroll flex flex-col-reverse">
+        {chatMessages.map((c, i) => (
+          <ChatMessage key={i} name={c.name} message={c.message} />
+        ))}
+      </div>
+
+      <form
+        className="mx-5 p-2 w-96 border border-zinc-800 rounded-lg flex"
+        onSubmit={(e) => {
+          e.preventDefault();
+          dispatch(
+            addMessage({
+              name: "Tom Jerry",
+              message: liveMessage,
+            })
+          );
+          setLiveMessage("");
+        }}
+      >
+        <input
+          type="text"
+          className="py-1 px-3 bg-transparent w-full border-b-[1px] border-zinc-500 text-white"
+          placeholder="Chat..."
+          value={liveMessage}
+          onChange={(e) => setLiveMessage(e.target.value)}
+        />
+        <button>
+          <span class="material-symbols-outlined text-zinc-500">send</span>
+        </button>
+      </form>
+    </>
   );
 };
 
